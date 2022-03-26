fc97e7f90720c172c0bfabad2871003262b71dc2
muhom <uslinskys@gmail.com>
2022-03-26 14:28:24 +0200

    Added func "write_block"

--- BlockChain/block.py
+++ BlockChain/block.py
@@ -1,3 +1,18 @@
+import json
 
 
-print(1234)
+def write_block(name, amount, to_whom, prev_hash=""):
+    data = {"name": name,
+            "amount": amount,
+            "to_whom": to_whom,
+            "hash": prev_hash}
+    with open("test", "w") as file:
+        json.dump(data, file, indent=4, ensure_ascii=False)
+
+
+def main():
+    write_block("Vadim", 10, "Klava")
+
+
+if __name__ == "__main__":
+    main()
