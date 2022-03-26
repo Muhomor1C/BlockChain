623bd649b0cf703a86901f953995e1075a85b086
muhom <uslinskys@gmail.com>
2022-03-26 16:08:46 +0200

    added checking files

--- BlockChain/block.py
+++ BlockChain/block.py
@@ -2,19 +2,29 @@ import json
 import os
 
 
+blockcain_directory = os.curdir + os.sep + "blocks" + os.sep
+if not os.path.exists(blockcain_directory):
+    os.mkdir(blockcain_directory)
+
+def get_filename():
+    files = sorted(int(i) for i in os.listdir(blockcain_directory))
+    try:
+        return int(files[-1])
+    except:
+        return -1
+
+
 def write_block(name, amount, to_whom, prev_hash=""):
-    blockcain_directory = os.curdir + os.sep + "blocks" + os.sep
+    file_name = get_filename()
     data = {"name": name,
             "amount": amount,
             "to_whom": to_whom,
             "hash": prev_hash}
-    with open(blockcain_directory + "test", "w") as file:
+    with open(blockcain_directory + str(file_name+1), "w") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
 
 def main():
-    if not os.path.exists(os.curdir + os.sep + "blocks" + os.sep):
-        os.mkdir(os.curdir + os.sep + "blocks" + os.sep)
     write_block("Vadim", 10, "Klava")
 
 
