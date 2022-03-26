2e25bc2d1a56242c4ad231064df22874507dee5f
muhom <uslinskys@gmail.com>
2022-03-26 20:32:34 +0200

    added automated create genesis block

--- BlockChain/block.py
+++ BlockChain/block.py
@@ -1,10 +1,9 @@
+import hashlib
 import json
 import os
 
+blockcain_directory = blockcain_directory = os.curdir + os.sep + "blocks" + os.sep
 
-blockcain_directory = os.curdir + os.sep + "blocks" + os.sep
-if not os.path.exists(blockcain_directory):
-    os.mkdir(blockcain_directory)
 
 def get_filename():
     files = sorted(int(i) for i in os.listdir(blockcain_directory))
@@ -12,6 +11,11 @@ def get_filename():
         return int(files[-1])
     except:
         return -1
+
+
+def get_hash(filename):
+    file = open(filename, "rb").read()
+    return hashlib.md5(file).hexdigest()
 
 
 def write_block(name, amount, to_whom, prev_hash=""):
@@ -23,10 +27,19 @@ def write_block(name, amount, to_whom, prev_hash=""):
     with open(blockcain_directory + str(file_name+1), "w") as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
+def create_genesis():
+    write_block("GENESIS", 0, "Genesis")
+
 
 def main():
-    write_block("Vadim", 10, "Klava")
+    prev_hash = str(get_hash(blockcain_directory + str(get_filename())))
+    write_block("Vadim", 10, "Klava", prev_hash)
 
 
 if __name__ == "__main__":
+    if not os.path.exists(blockcain_directory):
+        os.mkdir(blockcain_directory)
+        create_genesis()
+    if not os.path.exists(blockcain_directory + "0"):
+        create_genesis()
     main()
